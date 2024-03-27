86bb8b7caa00f891679617ab6a55bc0c78a89ae0
Koodohyeon <koodh6523@naver.com>
2024-03-27 16:17:45 +0900

    js 연습

--- src/practice/ch06_객체.js
+++ src/practice/ch06_객체.js
@@ -1,6 +1,6 @@
-const obj = {name: 'james', age:25, job:'proprammer'}
+const obj = {name:'james', age:25, job:'programmer'}
 let age = 'age';
-console.log(obj.name, obj[age],  obj['job']);
+console.log(obj.name, obj[age], obj['job']);
 
-for (let key in obj)      
-    console.log(obj[key]);
+for (let key in obj) 
+  console.log(obj[key]);
